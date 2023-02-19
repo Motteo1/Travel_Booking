eb2e763e904b1c50d9ee11416adf380f2af47898
Motteo1 <timothykirungu@gmail.com>
2023-02-19 13:37:15 +0300

    official secret key

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+# security against CSRF attacks
+app.config['SECRET_KEY'] = 'secret'
+
+login_user = LoginManager(app)
+login_user.login_view = 'login'
+login_user.login_message = 'Please login to access this page.'
+
+from app import routes
