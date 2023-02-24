c4b32c9c081332507f9817951d89721e1071844b
Motteo1 <timothykirungu@gmail.com>
2023-02-24 17:02:22 +0300

    add test

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,23 +1,24 @@
-"""create test cases for all methods in engine.py"""
-#         Base.metadata.create_all(self.__engine)
-#         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-#         self.__session = Session()
-#
-#     def close(self):
-#         """close session"""
-#         self.__session.close()
-#
-#     def get(self, cls, id):
-#         """get object by id"""
-#         if cls and id:
-#             key = cls + '.' + id
-#             if key in self.__objects:
-#                 return self.__objects[key]
-#         return None
-#
-#     def count(self, cls=None):
-#         """count number of objects in storage"""
-#         if cls:
-#             return len(self.all(cls))
-#         return len(self.all())
-#
+"""
+create test cases for all methods in engine.py
+"""
+Base.metadata.create_all(self.__engine)
+Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+self.__session = Session()
+
+def close(self):
+        """close session"""
+        self.__session.close()
+
+def get(self, cls, id):
+    """get object by id"""
+    if cls and id:
+        key = cls + '.' + id
+        if key in self.__objects:
+            return self.__objects[key]
+    return None
+
+def count(self, cls=None):
+    """count number of objects in storage"""
+    if cls:
+        return len(self.all(cls))
+    return len(self.all())
